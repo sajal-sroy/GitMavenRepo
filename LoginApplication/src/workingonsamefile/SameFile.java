37934089a31a374cfec254202e1a46b5763120c5
sajal sinha roy <sajal.sinha.roy@PDC4-LX-5464ZBC.dir.svc.accenture.com>
2016-09-02 11:45:19 +0530

    Changes done new branch
    Signed-off-by: sajal sinha roy <sajal.sinha.roy@PDC4-LX-5464ZBC.dir.svc.accenture.com>

--- LoginApplication/src/workingonsamefile/SameFile.java
+++ LoginApplication/src/workingonsamefile/SameFile.java
@@ -2,8 +2,11 @@ package workingonsamefile;
 
 public class SameFile {
   public void doLoop(){
+	  int count=0;
 	  for(int i=0;i<10;i++){
+		  count=count+i;
 		  System.out.println(i);
 	  }
+	  System.out.println(count/10 *100);
   }
 }
