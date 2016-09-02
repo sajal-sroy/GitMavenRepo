bbc1cb68191a62716219d7c54009362fb69cc9b9
sajal sinha roy <sajal.sinha.roy@PDC4-LX-5464ZBC.dir.svc.accenture.com>
2016-09-02 10:00:14 +0530

    Inital commit of Login Application
    Signed-off-by: sajal sinha roy <sajal.sinha.roy@PDC4-LX-5464ZBC.dir.svc.accenture.com>

---
+++ LoginApplication/src/com/LoginServ.java
@@ -0,0 +1,44 @@
+package com;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServ
+ */
+public class LoginServ extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServ() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname=request.getParameter("uname");
+		String pass=request.getParameter("pass");
+		if(uname.equalsIgnoreCase("admin") && pass.equalsIgnoreCase("admin")){
+			response.sendRedirect("success.jsp");
+		}
+		else{
+			response.sendRedirect("error.jsp");
+		}
+	}
+
+}
